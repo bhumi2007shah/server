8b90e0db3e342a10ed8be7358c59430b34fee56d
SumitBagul <sumit@litmusblox.io>
2019-07-08 11:28:26 +0530

    #6: Model -Update jobService

--- server/src/main/java/io/litmusblox/server/service/impl/JobService.java
+++ server/src/main/java/io/litmusblox/server/service/impl/JobService.java
@@ -4,13 +4,14 @@
 
 package io.litmusblox.server.service.impl;
 
+import io.litmusblox.server.Constant.IConstant;
 import io.litmusblox.server.model.Job;
 import io.litmusblox.server.repository.JobRepository;
+import io.litmusblox.server.repository.JobScreeningQuestionsRepository;
 import io.litmusblox.server.service.IJobService;
+import io.litmusblox.server.service.JobResponseBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * Implementation class for JobService
@@ -27,8 +28,55 @@ public class JobService implements IJobService {
     @Autowired
     JobRepository jobRepository;
 
+    @Autowired
+    JobScreeningQuestionsRepository jobScreeningQuestionsRepository;
+
     @Override
-    public List<Job> findAll() throws Exception {
-        return jobRepository.findAll();
+    public JobResponseBean addJob(Job job, String pageName) {//add job with respective pageName
+
+        Job oldJob = null;
+        if(null!=job.getId()){
+             oldJob = jobRepository.findById(job.getId()).orElse(new Job());
+        }
+
+        if(pageName.equalsIgnoreCase(IConstant.OVERVIEW)){
+            return addJobOverview(job,oldJob);
+        }else if(pageName.equalsIgnoreCase(IConstant.SCREENING_QUESTIONS)){
+            return addJobScreeningQuestions(job, oldJob);
+        }
+        return null;
+    }
+
+    private JobResponseBean addJobOverview(Job job, Job oldJob) { //method for add job for Overview page
+
+        String title=job.getJobTitle();
+        //validate title
+       if(title.length()>IConstant.TITLE_MAX_LENGTH){  //Truncate job title if it is greater than max length
+            title=title.substring(0,IConstant.TITLE_MAX_LENGTH);
+        }
+
+        if(null!=oldJob){//only update existing job
+            //set job id from the db object
+            job.setId(oldJob.getId());
+            jobRepository.save(job);
+        }else{ //Create new entry for job
+            jobRepository.save(job);
+        }
+        JobResponseBean jb=new JobResponseBean();
+        jb.setJobId(job.getId());
+        return jb;
+        //return new JobResponseBean(job.getId());
+    }
+
+    private JobResponseBean addJobScreeningQuestions(Job job, Job oldJob){ //method for add screening questions
+
+        if(null!=oldJob.getJobScreeningQuestionsList()){
+            jobScreeningQuestionsRepository.deleteAll(oldJob.getJobScreeningQuestionsList());//delete old job screening question list
+        }
+        oldJob.getJobScreeningQuestionsList().addAll(job.getJobScreeningQuestionsList());//add new screening question list
+        jobRepository.save(oldJob);
+        JobResponseBean jb=new JobResponseBean();
+        jb.setJobId(job.getId());
+        return jb;
     }
 }

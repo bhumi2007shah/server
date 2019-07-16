a0b479a8e32f7013d5479c9e8b2bf99d70554bc6
shital <shital@hexagonsearch.com>
2019-07-16 16:22:25 +0530

    Added a simulator for ML api call to occur every few seconds

--- server/src/main/java/io/litmusblox/server/model/JobCapabilities.java
+++ server/src/main/java/io/litmusblox/server/model/JobCapabilities.java
@@ -28,7 +28,7 @@ public class JobCapabilities implements Serializable {
 
     @Id
     @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull

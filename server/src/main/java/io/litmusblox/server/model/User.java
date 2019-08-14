0fb9617dc0538cdd2e1660baeddac334d710db36
shital <shital@hexagonsearch.com>
2019-08-14 13:55:23 +0530

    #85: Super Admin privileges

--- server/src/main/java/io/litmusblox/server/model/User.java
+++ server/src/main/java/io/litmusblox/server/model/User.java
@@ -63,7 +63,7 @@ public class User implements Serializable, UserDetails {
 
     @NotNull
     @Column(name = "ROLE")
-    private String role = IConstant.UserRole.Names.RECRUITER;
+    private String role;
 
     @Column(name = "DESIGNATION")
     private String designation;

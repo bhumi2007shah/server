ee5c5a3d7d71da473aa7e881b96061b1e81177b9
sameer khan <sameer@hexagonsearch.com>
2019-09-18 16:35:35 +0530

    Test files

--- server/src/test/java/io/litmusblox/server/ServerApplicationTests.java
+++ server/src/test/java/io/litmusblox/server/ServerApplicationTests.java
@@ -1,18 +1,18 @@
 package io.litmusblox.server;
 
+import io.litmusblox.server.security.JwtTokenUtil;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Ignore
 public class ServerApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }

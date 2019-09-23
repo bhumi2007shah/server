5e2c96c6ab8ddb8e00d735304bdc7348bc2c0157
sameer khan <sameer@hexagonsearch.com>
2019-09-23 14:44:07 +0530

    added @Ignore to ignore Test classes.

--- server/src/test/java/io/litmusblox/server/controller/AuthControllerTest.java
+++ server/src/test/java/io/litmusblox/server/controller/AuthControllerTest.java
@@ -28,6 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Class Name : AuthControllerTest
  * Project Name : server
  */
+@Ignore
 @NoArgsConstructor
 @RunWith(SpringRunner.class)
 @WebMvcTest(AuthController.class)

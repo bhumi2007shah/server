262c377ae1637e076ebeb68640461bf3232bb63b
shital <shital@hexagonsearch.com>
2019-11-27 13:33:24 +0530

    #243 1. Changed the mapping for jobview by stage api to GET 2. Added jobStageStep filters in various controller methods

--- server/src/main/java/io/litmusblox/server/controller/JobController.java
+++ server/src/main/java/io/litmusblox/server/controller/JobController.java
@@ -90,7 +90,7 @@ public class JobController {
      * @return response bean with all details as a json string
      * @throws Exception
      */
-    @PostMapping(value = "/jobViewByStage/{jobId}/{stage}")
+    @GetMapping(value = "/jobViewByStage/{jobId}/{stage}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     String getJobViewByIdAndStage(@PathVariable ("jobId") Long jobId, @PathVariable ("stage") String stage) throws Exception {
@@ -101,6 +101,7 @@ public class JobController {
                     put("User",Arrays.asList("displayName"));
                     put("CvRating", Arrays.asList("overallRating"));
                     put("CandidateEducationDetails", Arrays.asList("degree"));
+                    put("JobStageStep", new ArrayList<>(0));
                 }}),
                 (new HashMap<String, List<String>>(){{
                     put("Job",Arrays.asList("jobDescription","jobScreeningQuestionsList","jobKeySkillsList","jobCapabilityList", "updatedOn", "updatedBy"));
